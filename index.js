a4f2d193f4cafea189fffdb5fd63f77b275d4296
DaniloPnggg <danilostvs@gmail.com>
2024-05-11 17:00:09 -0300

    intercurso

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const input = document.getElementById(`textbox`).value
-
+alert(`sexo sexo sexo sexo sexo eu gosto de fazer SEXO eu gosto MUITO de fazer SEXO`)
 if (input.value === `/help`) {
     //teste
 }
